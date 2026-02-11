2214c47e03929c86808ffa73b83e2cf172ca339f
cchan555 <codychan216@yahoo.com>
2026-02-11 10:21:06 -0600

    Lab1, lab2, and lab3 functions finalized.

--- R/Lab3_Plot.R
+++ R/Lab3_Plot.R
@@ -1,19 +1,15 @@
-#' Creates a plot with blue dots
+#' Creates a plot with blue dots with limits
 #'
 #' @param xVariable x-axis variables
 #' @param yVariable y-axix variables
-#' @param xlab x label
-#' @param ylab y label
+#' @param xlabel x label
+#' @param ylabel y label
 #' @param title title of the plot
-#' @param dataType dataset being called in
 #'
 #' @returns returns the plot
 #' @export
-Lab3_Plot <- function(xVariable, yVariable, xlabel, ylabal, title, datatype) {
-  p <- plot(x = xVariable, y = yVariable, xlab = xlabel, ylab = ylabel,
-        main = "Spruce Height Prediction", data = datatype,
-        bg = "blue", pch = 21, cex = 1.2, ylim = c(0, 1.1*max(yVariable)),
-        xlim = c(0, 1.1*max(xVariable)))
-  return (p)
+Lab3_Plot <- function(xVariable, yVariable, xlabel, ylabel, title) {
+  plot(x = xVariable, y = yVariable, xlab = xlabel, ylab = ylabel, main = title,
+       bg = "blue", pch = 21, cex = 1.2, ylim = c(0, 1.1*max(yVariable)),
+       xlim = c(0, 1.1*max(xVariable)))
 }
-
