ca38d371d3ce3f2f9a8f73a517f4ceeccd7d9612
cchan555 <codychan216@yahoo.com>
2026-02-17 14:19:35 -0600

    Standard Deviation function updated

--- R/mysd.R
+++ R/mysd.R
@@ -1,14 +1,17 @@
-#' Title
+#' Title My Standard Deviation Function
 #'
 #' @param x A quantitative vector
-#' @param na.rm A Value
+#' @param na.rm A Logical indicating whether to remove NA value before computation
+#' @importFrom stats var
 #'
-#' @returns A quantitative vector with square
+#' @returns A quantitative vector with square (SD of the numeric vector)
 #' @export
 #'
+#'@examples
+#' mysd(c(1, 2, 3, 4, 5))
 mysd <- function(x, na.rm = TRUE) {
     if(na.rm) {
-      x<- x[!is.na(x)]
+      x <- x[!is.na(x)]
     }
   sqrt(var(x))
 }
